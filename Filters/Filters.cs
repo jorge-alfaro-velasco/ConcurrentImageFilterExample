8637dfd669129b643a274233315ca0389a38bfd7
jorge-alfaro-velasco <jorge.alfaro.velasco@gmail.com>
2019-02-20 09:39:38 -0600

    Inicial example

---
+++ Filters/Filters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// For image management
+using System.Drawing;
+
+namespace Filters
+{
+    /// <summary>
+    /// Gray scale methods
+    /// </summary>
+    public class Filters
+    {
+        /// <summary>
+        /// Apply gray scale filter
+        /// </summary>
+        /// <param name="source">image location</param>
+        /// <returns>Image in gray scale</returns>
+        public static Bitmap GrayScale(Bitmap source)
+        {
+            Bitmap bm = new Bitmap(source.Width, source.Height);
+            for (int y = 0; y < bm.Height; y++)
+            {
+                for (int x = 0; x < bm.Width; x++)
+                {
+                    Color c = source.GetPixel(x, y);
+                    int average = (Convert.ToInt32(c.R) + Convert.ToInt32(c.G) + Convert.ToInt32(c.B)) / 3;
+                    bm.SetPixel(x, y, Color.FromArgb(average, average, average));
+                }
+            }
+            return bm;
+        }
+    }
+}
---
+++ ImagesConcurrentExample/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Namespace for get some statistics 
+using System.Diagnostics;
+// Allow us to access into HD/SSD
+using System.IO;
+// For image management
+using System.Drawing;
+
+
+
+namespace ImagesConcurrentExample
+{
+    class Program
+    {
+        #region Global variables
+        //The arroba at the beginning is for indicate to .NET that backslash character belongs to string
+        static string imagePath = @"E:\Images";
+        // Path for "filtering" result (new images)
+        static string newImagePath = @"E:\NewImages";
+        //For get the execution time
+        static Stopwatch stopWatch = new Stopwatch();
+        #endregion Global variables
+
+        static void Main(string[] args)
+        {
+            SecuencialMethod();            
+            ParallelMethod();
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Apply the gray scale filter in secuencial way
+        /// </summary>
+        static void SecuencialMethod()
+        {
+            //Start to count time
+            stopWatch.Start();
+
+            //Secuencial method that apply grayscale filter to whole folder (images)
+            foreach (string file in Directory.GetFiles(imagePath))
+            {
+                Bitmap bitmap = (Bitmap)Image.FromFile(file);
+                bitmap = Filters.Filters.GrayScale(bitmap);
+                bitmap.Save(newImagePath + "\\" + Path.GetFileNameWithoutExtension(file) + Guid.NewGuid().ToString() + ".jpg");
+            }
+            Console.WriteLine("Secuencial Foreah - execution time = {0} seconds\n", stopWatch.Elapsed.TotalSeconds);
+            stopWatch.Reset();
+        }
+
+        /// <summary>
+        /// Apply the gray scale filter in secuencial way
+        /// </summary>
+        static void ParallelMethod()
+        {
+            stopWatch.Start();
+
+            //Secuencial method that apply grayscale filter to whole folder (images)
+            Parallel.ForEach(Directory.GetFiles(imagePath), file =>
+            {
+                Bitmap bitmap = (Bitmap)Image.FromFile(file);
+                bitmap = Filters.Filters.GrayScale(bitmap);
+                bitmap.Save(newImagePath + "\\" + Path.GetFileNameWithoutExtension(file) + Guid.NewGuid().ToString() + ".jpg");
+            });
+            Console.WriteLine("Parallel Foraech - execution time = {0} seconds\n", stopWatch.Elapsed.TotalSeconds);
+            stopWatch.Reset();
+        }
+    }
+}
